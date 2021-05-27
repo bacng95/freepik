b7e6edca3824614572f96746931b3c1995728580
Bac Nguyen <bac.nguyen.71271@gmail.com>
2021-05-28 03:07:41 +0700

    update

---
+++ app/Controllers/Http/FreepikController.js
@@ -0,0 +1,226 @@
+'use strict'
+
+const axios = use('axios')
+const Database = use('Database');
+
+class FreepikController {
+
+    async refreshCsrfToken () {
+        let cookie = await this._getCookie()
+        
+        let resp = await axios.get('https://www.freepik.com/home', {
+            headers: {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.148 Safari/537.36',
+                'upgrade-insecure-requests': 1,
+                'referer': '',
+                'cookie': this._cookieObjectToString(cookie),
+                'accept-encoding': 'gzip, deflate, br',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'x-csrf-token': cookie['csrf_freepik'],
+                'x-requested-with': 'XMLHttpRequest'
+            },
+            maxRedirects: 0
+        })
+        this._mergeCookie(this._parseCookieBrower(resp.headers['set-cookie']), cookie)
+        await this._setCookie(cookie)
+        console.log('Refresh done !')
+    }
+
+    async getLinkAction({request, response}) {
+        const { url } = request.all()
+        let itemId = ''
+        if (url && url != '') {
+            // Get ID of link
+            itemId = url.toString().replace('.htm', '')
+            itemId = itemId.split('_')
+            itemId = itemId[itemId.length - 1]
+        }
+
+        let link = await this.getLink(itemId)
+
+        if (link) {
+            return response.json({
+                code: 1,
+                message: 'Ngon rồi đại ca !',
+                data: link
+            })
+        } else {
+            return response.json({
+                code: 0,
+                message: 'Có lỗi rồi đại ca ơi !',
+            })
+        }
+    }
+
+    async getInfoAccount({request, response}) {
+        let cookie = await this._getCookie()
+
+        let resp = await axios.get('https://www.freepik.com/xhr/validate', {
+            headers: {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.148 Safari/537.36',
+                'upgrade-insecure-requests': 1,
+                'referer': 'https://www.freepik.com/premium-psd/blank-photo-frame-mockup-design_13817478.htm',
+                'cookie': this._cookieObjectToString(cookie),
+                'accept-encoding': 'gzip, deflate, br',
+                'x-csrf-token': cookie['csrf_freepik'],
+                'x-requested-with': 'XMLHttpRequest'
+            },
+            maxRedirects: 0
+        })
+
+        this._mergeCookie(this._parseCookieBrower(resp.headers['set-cookie']), cookie)
+        await this._setCookie(cookie)
+
+        return response.json({
+            code: 0,
+            message: 'Ngon rồi đại ca!',
+            data: resp.data
+        })
+    }
+
+    async getLink(item_id) {
+        
+        if (item_id) {
+            let cookie = await this._getCookie()
+        
+            let resp = await axios.get(`https://www.freepik.com/xhr/register-download/${item_id}`, {
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.148 Safari/537.36',
+                    'upgrade-insecure-requests': 1,
+                    'referer': 'https://www.freepik.com/',
+                    'cookie': this._cookieObjectToString(cookie),
+                    'accept-encoding': 'gzip, deflate, br',
+                    'x-csrf-token': cookie['csrf_freepik'],
+                    'x-requested-with': 'XMLHttpRequest'
+                },
+                maxRedirects: 0
+            })
+    
+            this._mergeCookie(this._parseCookieBrower(resp.headers['set-cookie']), cookie)
+    
+            try {
+                let resp3 = await axios.get(`https://www.freepik.com/download-file/${item_id}?is_premium_item=0&is_premium_user=1`, {
+                    headers: {
+                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.148 Safari/537.36',
+                        'upgrade-insecure-requests': 1,
+                        'referer': `https://www.freepik.com/premium-psd/blank-photo-frame-mockup-design_${item_id}.htm`,
+                        'cookie': this._cookieObjectToString(cookie),
+                        'accept-encoding': 'gzip, deflate, br',
+                        'x-csrf-token': cookie['csrf_freepik'],
+                        'x-requested-with': 'XMLHttpRequest'
+                    },
+                    maxRedirects: 0
+                })
+    
+                this._mergeCookie(this._parseCookieBrower(resp3.headers['set-cookie']), cookie)
+                await this._setCookie(cookie)
+                
+                return '';
+            } catch (error) {
+    
+                await this._setCookie(cookie)
+                this._mergeCookie(this._parseCookieBrower(error.response.headers['set-cookie']), cookie)
+    
+                console.log(error.response.headers.location);
+    
+                return error.response.headers.location
+            }
+        }
+        
+    }
+
+    _cookieObjectToString(cookie) {
+        let _cookie = ''
+        if (typeof cookie === "object") {
+            Object.keys(cookie).forEach(function(key) {
+                _cookie += `${key}=${cookie[key]}; `
+            });
+        }
+
+        return _cookie
+    }
+
+    _csrfCookie(cookieString) {
+        if (cookieString.indexOf('csrf_freepik')) {
+            let csrf = cookieString.split(';')[0]
+            csrf = csrf.split('=')[0]
+            return `csrf_freepik=${csrf}`
+        }
+        return ''
+    }
+
+    _mergeCookie(cookie, CookieObject) {
+        Object.keys(cookie).forEach(function(key1) {
+            // CookieObject[key] = 
+            let check = false;
+            Object.keys(CookieObject).forEach(function(key2) {
+                if (key1 == key2) {
+                    CookieObject[key2] = cookie[key1]
+                    check = true
+                }
+            });
+
+            if (!check) {
+                CookieObject[key1] = cookie[key1]
+            }
+
+        });
+    }
+
+    
+    async _setCookie(cookie) {
+        let _cookie = ''
+        if (typeof cookie === "object") {
+            Object.keys(cookie).forEach(function(key) {
+                _cookie += `${key}=${cookie[key]}; `
+            });
+        }
+        let cookieSaveStage = await Database.table('settings')
+        .where('meta_key', 'freepik_cookie')
+        .update({
+            'data': _cookie
+        })
+
+        if (cookieSaveStage) {
+            return true
+        }
+        return false
+    }
+
+    async _getCookie() {
+        const CookieSetting = await Database.table('settings')
+        .where('meta_key', 'freepik_cookie')
+        .first()
+
+        if (CookieSetting && CookieSetting.data) {
+            return this._parseCookies(CookieSetting.data)
+        }
+
+        return {}
+    }
+
+    _parseCookieBrower(cookie) {
+        let _cookie = {}
+        cookie.forEach(element => {
+            let cookieItem = element.split(';')[0]
+            cookieItem = cookieItem.split('=')
+            _cookie[cookieItem[0]] = cookieItem[1]
+        });
+
+        return _cookie
+    }
+
+    _parseCookies (cookieString) {
+        var list = {}
+    
+        cookieString && cookieString.split(';').forEach(function( cookie ) {
+            var parts = cookie.split('=');
+            if (parts[0] !== '')
+                list[parts.shift().trim()] = parts.join('=');
+        });
+    
+        return list;
+    }
+}
+
+module.exports = FreepikController
---
+++ app/Tasks/Refresh.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Task = use('Task')
+const freePik = require('../Controllers/Http/FreepikController')
+
+class Refresh extends Task {
+  static get schedule () {
+    return '* * * * *'
+  }
+
+  async handle () {
+    console.log('Run refresh');
+    const fp = new freePik
+    fp.refreshCsrfToken()
+  }
+}
+
+module.exports = Refresh
